11acd25317d75a7ea23366e8104ecb005dda2448
Parker Jones <parker.matthewjones@gmail.com>
2022-02-28 11:04:32 -0600

    round 1: fight

--- src/assets/Models/OasisModel.js
+++ src/assets/Models/OasisModel.js
@@ -1,8 +1,8 @@
 import oasis from "./oasis.glb";
 import * as THREE from "three";
 import {
-  useThree,
-  useFrame,
+  // useThree,
+  // useFrame,
   Canvas,
   extend,
   useLoader,
@@ -11,47 +11,47 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
 import oasisBaked from "../materials/Oasisbake.jpg";
 import { Water } from "three/examples/jsm/objects/Water.js";
-import { useRef, useMemo } from "react";
+// import { useRef, useMemo } from "react";
 
 extend({ Water });
 
-function Ocean() {
-  const ref = useRef();
-  const gl = useThree((state) => state.gl);
-  const waterNormals = useLoader(
-    THREE.TextureLoader,
-    "https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/waternormals.jpg"
-  );
+// function Ocean() {
+//   const ref = useRef();
+//   const gl = useThree((state) => state.gl);
+//   const waterNormals = useLoader(
+//     THREE.TextureLoader,
+//     "https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/waternormals.jpg"
+//   );
 
-  waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
-  const geom = useMemo(() => new THREE.PlaneGeometry(30000, 30000), []);
-  const config = useMemo(
-    () => ({
-      textureWidth: 512,
-      textureHeight: 512,
-      waterNormals,
-      sunDirection: new THREE.Vector3(),
-      sunColor: 0xeb8934,
-      waterColor: 0x0064b5,
-      distortionScale: 40,
-      fog: false,
-      format: gl.encoding,
-    }),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [waterNormals]
-  );
-  useFrame(
-    (state, delta) => (ref.current.material.uniforms.time.value += delta)
-  );
-  return (
-    <water
-      ref={ref}
-      args={[geom, config]}
-      rotation-x={-Math.PI / 2}
-      position={[0, 0, 0]}
-    />
-  );
-}
+//   waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
+//   const geom = useMemo(() => new THREE.PlaneGeometry(30000, 30000), []);
+//   const config = useMemo(
+//     () => ({
+//       textureWidth: 512,
+//       textureHeight: 512,
+//       waterNormals,
+//       sunDirection: new THREE.Vector3(),
+//       sunColor: 0xeb8934,
+//       waterColor: 0x0064b5,
+//       distortionScale: 40,
+//       fog: false,
+//       format: gl.encoding,
+//     }),
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//     [waterNormals]
+//   );
+//   useFrame(
+//     (state, delta) => (ref.current.material.uniforms.time.value += delta)
+//   );
+//   return (
+//     <water
+//       ref={ref}
+//       args={[geom, config]}
+//       rotation-x={-Math.PI / 2}
+//       position={[0, 0, 0]}
+//     />
+//   );
+// }
 
 export default function Oasis() {
   const bakedTexture = useLoader(TextureLoader, oasisBaked);
@@ -83,7 +83,7 @@ export default function Oasis() {
               />
             </mesh>
           </group>
-          <Ocean />
+          {/* <Ocean /> */}
         </Canvas>
       </div>
     </>
